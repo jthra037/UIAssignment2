b2c85d22db61ca90857df3784a5b8acaf0619307
jthra037 <jthra037@users.noreply.github.com>
2016-04-12 11:07:33 -0400

    Add borders to buttons. Add text to buttons. Add text component to button objects.

--- buttonController.js
+++ buttonController.js
@@ -1,31 +1,259 @@
+// set up for using canvas
 var c = document.getElementById("myCanvas");
+c.addEventListener( "click", getClickPosition);
+window.addEventListener( "keydown", debugShortcut, true);
 var ctx = c.getContext("2d");
 
-var mainMenu = [[200,20,400,50], // play
-		[200,90,400,50], // settings
-		[200,160,190,50], // credits
-		[410,160,190,50]]; // quit
+// store current and previous screen arrays
+var currentScreen;
+var prevScreen;
 
-var inPlay = [[5,5,100,50]]; // pause button
+var Button = function(x, y, w, h, text) // define a custom button class for canvas
+{
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.text = text;
+}
 
-var settingsMenu = [[5,5,100,50], // return
-		    [200,160,190,50]]; // control toggle
+// define all the buttons in the game, by screen
+// splash screen ss screen
+var ssScreenBtn = new Button(0,0,c.width,c.height,
+			     "Splash Screen");
+ssScreenBtn.click = function()
+{
+    currentScreen = mainMenu;
+}
+// main menu mm screen
+var mmPlayBtn = new Button(200,20,400,50,
+			  "Play");
+mmPlayBtn.click = function()
+{
+    currentScreen = inPlay;
+}
+var mmSettingsBtn = new Button(200,90,400,50,
+			      "Settings");
+mmSettingsBtn.click = function()
+{
+    currentScreen = settingsMenu;
+    prevScreen = mainMenu;
+}
+var mmCreditsBtn = new Button(200,160,190,50,
+			     "Credits");
+mmCreditsBtn.click = function()
+{
+    currentScreen = creditScreen;
+}
+var mmQuitBtn = new Button(410,160,190,50,
+			   "Quit");
+mmQuitBtn.click = endGame;
+// in play ip screen
+var ipPauseBtn = new Button(5,5,100,50,
+			   "Pause");
+ipPauseBtn.click = function()
+{
+    currentScreen = pauseMenu;
+    prevScreen = inPlay;
+}
+// settings menu sm screen
+var smReturnBtn = new Button(5,5,100,50,
+			    "Return");
+smReturnBtn.click = function()
+{
+    currentScreen = prevScreen;
+}
+var smControlBtn = new Button(200,160,190,50,
+			     "Controls");
+smControlBtn.click = function()
+{
 
-var pauseMenu = [[200,40,400,100], // return to game
-		[200,160,190,50], // settings
-		[410,160,190,50]]; // quit
+}
+// pause menu pm screen
+var pmReturnBtn = new Button(200,40,400,100,
+			    "Return");
+pmReturnBtn.click = function()
+{
+    currentScreen = inPlay;
+}
+var pmSettingsBtn = new Button(200,160,190,50,
+			      "Settings");
+pmSettingsBtn.click = function()
+{
+    currentScreen = settingsMenu;
+    prevScreen = pauseMenu;
+}
+var pmQuitBtn = new Button(410,160,190,50,
+			  "Quit");
+pmQuitBtn.click = function()
+{
+    currentScreen = mainMenu;
+}
+// game over go screen
+var goCreditsBtn = new Button(200,20,400,50,
+			     "Credits");
+goCreditsBtn.click = function()
+{
+    currentScreen = creditScreen;
+}
+var goPlayBtn = new Button(200,280,190,50,
+			  "Play");
+goPlayBtn.click = function()
+{
+    currentScreen = inPlay;
+}
+var goQuitBtn = new Button(410,280,190,50,
+			  "Quit");
+goQuitBtn.click = function()
+{
+    currentScreen = mainMenu;
+}
+// credit screen cs screen
+var csScreenBtn = new Button(0,0,c.width,c.height,
+			    "Credits");
+csScreenBtn.click = function()
+{
+    currentScreen = mainMenu;
+}
 
-var gameOverMenu = [[200,20,400,50], // credits
-		    [200,280,190,100], // play again
-		    [410,280,190,100]]; //quit
+// store each screen in an array
+var mainMenu = [mmPlayBtn, // play
+		mmSettingsBtn, // settings
+		mmCreditsBtn, // credits
+		mmQuitBtn]; // quit
 
+var inPlay = [ipPauseBtn]; // pause button
+
+var settingsMenu = [smReturnBtn, // return
+		    smControlBtn]; // control toggle
+
+var pauseMenu = [pmReturnBtn, // return to game
+		pmSettingsBtn, // settings
+		pmQuitBtn]; // quit
+
+var gameOverMenu = [goCreditsBtn, // credits
+		    goPlayBtn, // play again
+		    goQuitBtn]; //quit
+
+var splashScreen = [ssScreenBtn]; // clickable splash screen
+
+var creditScreen = [csScreenBtn]; // clickable credit screen
+
+// this function draws buttons on each scene in the canvas
 function drawMenu(thisMenu)
 {
+    ctx.clearRect(0,0,c.width,c.height);
+
     for (var i = 0; i < thisMenu.length; i++)
     {
+	// directions for button appearance
+	ctx.beginPath();
+	ctx.rect(thisMenu[i].x,thisMenu[i].y,thisMenu[i].w,thisMenu[i].h);
 	ctx.fillStyle = "blue";
-	ctx.fillRect(thisMenu[i][0],thisMenu[i][1],thisMenu[i][2],thisMenu[i][3]);
+	ctx.fill();
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+	// directions for button text
+	ctx.fillStyle = "black";
+	ctx.font = "20px Arial";
+	ctx.fillText(thisMenu[i].text, 
+		     thisMenu[i].x + 10, 
+		     thisMenu[i].y + (thisMenu[i].h/2) + 10);
     }
 }
 
-drawMenu();
+// this function gets corrected click positions within the canvas
+// and acts on the resulting collision with button elements
+function getClickPosition(e) 
+{
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x;
+    var yPosition = e.clientY - parentPosition.y;
+    var clicked = checkCollision(xPosition, yPosition);
+    if (clicked != -1)
+    {
+	currentScreen[clicked].click();
+	drawMenu(currentScreen);
+    }
+}
+ 
+// find the corrected click position
+function getPosition(element) 
+{
+    var xPosition = 0;
+    var yPosition = 0;
+      
+    while (element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return { x: xPosition, y: yPosition };
+}
+
+// check for button element collision based on corrected click position
+function checkCollision(xPos, yPos)
+{
+    var flag;
+    var left;
+    var right;
+    var top;
+    var bottom;
+
+    for (var i = 0; i < currentScreen.length; i++)
+    {
+	left = currentScreen[i].x;
+	right = left + currentScreen[i].w;
+	top = currentScreen[i].y;
+	bottom = top + currentScreen[i].h;
+	
+	//console.log(left+','+top+','+right+','+bottom);
+
+	if ((xPos >= left) &&
+	    (xPos <= right) &&
+	    (yPos >= top) &&
+	    (yPos <= bottom))
+	{
+	    return i;
+	}
+    }
+    return -1;
+}
+
+// clear the screen at the end of the game
+function endGame()
+{
+    ctx.clearRect(0,0,c.width,c.height);
+}
+
+// provide debug shortcuts for dev to quickly change screens
+function debugShortcut(e)
+{
+    console.log(e.keyCode);
+    
+    if (e.keyCode == 82)
+    {
+	currentScreen = inPlay;
+	drawMenu(currentScreen);
+    }
+    if (e.keyCode == 88)
+    {
+	currentScreen = gameOverMenu;
+	drawMenu(currentScreen);
+    }
+    if (e.keyCode == 32)
+    {
+	currentScreen = splashScreen;
+	drawMenu(currentScreen);
+    }
+    if (e.keyCode == 77)
+    {
+	currentScreen = mainMenu;
+	drawMenu(currentScreen);
+    }
+}
+
+// show the splash screen first
+currentScreen = splashScreen;
+// and enter the draw loop
+drawMenu(currentScreen);
